ce19e0aa94fafa2b8d14511386fe135cd07312c7
jose <reynosojose2005@gmail.com>
2023-09-29 08:34:28 -0600

    inicio

---
+++ src/main/java/universidadPolitecnica/UniversidadPolitecnicaApplication.java
@@ -0,0 +1,13 @@
+package universidadPolitecnica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversidadPolitecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversidadPolitecnicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/universidadPolitecnica/UniversidadPolitecnicaApplicationTests.java
@@ -0,0 +1,13 @@
+package universidadPolitecnica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversidadPolitecnicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
